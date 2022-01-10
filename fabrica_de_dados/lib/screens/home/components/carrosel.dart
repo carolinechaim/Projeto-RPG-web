d597fbfdc1f83e0fbc1f3d17142abae51aa80a97
carolinechaim <caroline.chaim@gmail.com>
2022-01-10 12:03:31 -0300

    Detalhes no carrosel

--- fabrica_de_dados/lib/screens/home/components/carrosel.dart
+++ fabrica_de_dados/lib/screens/home/components/carrosel.dart
@@ -23,7 +23,7 @@ class _CarroselState extends State<Carrosel> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
+    return Stack(children: [
       CarouselSlider(
         carouselController: buttonCarouselController,
         options: CarouselOptions(
@@ -39,73 +39,75 @@ class _CarroselState extends State<Carrosel> {
           },
         ),
         items: images
-            .map((e) => Stack(children: [
-                  ClipRRect(
-                      child: Stack(
-                    fit: StackFit.loose,
-                    children: <Widget>[
-                      Image.network(e,
-                          width: 2050, height: 700, fit: BoxFit.cover)
-                    ],
-                  )),
-                  Positioned(
-                      left: MediaQuery.of(context).size.width / 300,
-                      top: MediaQuery.of(context).size.height / 3.25,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all<Color>(Colors.white)),
-                        onPressed: () => buttonCarouselController.previousPage(
-                            duration: const Duration(milliseconds: 500),
-                            curve: Curves.linear),
-                        child: const Icon(
-                          Icons.arrow_back_ios_new_outlined,
-                          color: Colors.black,
-                          size: 36.0,
-                        ),
-                      )),
-                  Positioned(
-                      left: MediaQuery.of(context).size.width / 1.05,
-                      top: MediaQuery.of(context).size.height / 3.25,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all<Color>(Colors.white)),
-                        onPressed: () => buttonCarouselController.nextPage(
-                            duration: const Duration(milliseconds: 300),
-                            curve: Curves.linear),
-                        child: const Icon(
-                          Icons.arrow_forward_ios_outlined,
-                          color: Colors.black,
-                          size: 36.0,
-                        ),
-                      ))
-                ]))
+            .map(
+              (e) => ClipRRect(
+                  child: Stack(
+                fit: StackFit.loose,
+                children: <Widget>[
+                  Image.network(e, width: 2050, height: 700, fit: BoxFit.cover)
+                ],
+              )),
+            )
             .toList(),
       ),
-      Container(
-          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-          decoration: BoxDecoration(
-            color: Colors.amber,
-            borderRadius: BorderRadius.circular(5),
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: images.map((e) {
-              int index = images.indexOf(e);
-              return Container(
-                  width: 8.0,
-                  height: 8.0,
-                  margin: const EdgeInsets.symmetric(
-                      vertical: 5.0, horizontal: 4.0),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: _current == index
-                          ? const Color.fromRGBO(0, 0, 0, 0.9)
-                          : const Color.fromRGBO(0, 0, 0, 0.4)));
-            }).toList(),
-          ))
+      Positioned(
+          left: MediaQuery.of(context).size.width / 300,
+          top: MediaQuery.of(context).size.height / 3.25,
+          child: ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.white)),
+            onPressed: () => buttonCarouselController.previousPage(
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.linear),
+            child: const Icon(
+              Icons.arrow_back_ios_new_outlined,
+              color: Colors.black,
+              size: 36.0,
+            ),
+          )),
+      Positioned(
+          left: MediaQuery.of(context).size.width / 1.05,
+          top: MediaQuery.of(context).size.height / 3.25,
+          child: ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.white)),
+            onPressed: () => buttonCarouselController.nextPage(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.linear),
+            child: const Icon(
+              Icons.arrow_forward_ios_outlined,
+              color: Colors.black,
+              size: 36.0,
+            ),
+          )),
+      Positioned(
+          left: MediaQuery.of(context).size.width / 2.1,
+          top: MediaQuery.of(context).size.height / 1.6,
+          child: Container(
+              padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+              decoration: BoxDecoration(
+                color: const Color(0xFFC4C4C4),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: images.map((e) {
+                  int index = images.indexOf(e);
+                  return Container(
+                      width: 8.0,
+                      height: 8.0,
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 5.0, horizontal: 4.0),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _current == index
+                              ? const Color.fromRGBO(0, 0, 0, 0.9)
+                              : Colors.white));
+                }).toList(),
+              )))
     ]);
   }
 }
