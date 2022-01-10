2d5380c0377ede6d412f27f7bab3bfce6bb0ae89
carolinechaim <caroline.chaim@gmail.com>
2022-01-10 11:41:22 -0300

    detalhes do carrosel

--- fabrica_de_dados/lib/components/header.dart
+++ fabrica_de_dados/lib/components/header.dart
@@ -12,68 +12,68 @@ class _HeaderState extends State<Header> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 90,
-      width: double.infinity,
-      color: Colors.white,
-      margin: const EdgeInsets.only(top: 5.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Container(width: 65, child: const DropDown()),
-          Container(
-              height: 45,
-              width: 270,
-              color: Colors.purple[200],
-              child: const Align(
-                  alignment: Alignment.center,
-                  child: Text("30% off toda sexta feira",
-                      style: TextStyle(fontSize: 20)))),
-          Container(
-            //color: Colors.amber,
-            child: const Text(
-              "Fabrica de Dados",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        height: 90,
+        width: double.infinity,
+        color: Colors.white,
+        margin: const EdgeInsets.only(top: 5.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(width: 65, child: const DropDown()),
+            Container(
+                height: 45,
+                width: 270,
+                color: Colors.purple[200],
+                child: const Align(
+                    alignment: Alignment.center,
+                    child: Text("30% off toda sexta feira",
+                        style: TextStyle(fontSize: 20)))),
+            Container(
+              //color: Colors.amber,
+              child: const Text(
+                "Fabrica de Dados",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
             ),
-          ),
-          Container(
-            //color: Colors.green,
-            child: const Text("loja"),
-          ),
-          Container(
-            //color: Colors.blue,
-            child: const Text("sobre nos"),
-          ),
-          Container(
-          width: 73,
-          height: 76,
-          decoration: new BoxDecoration(
-          image: new DecorationImage(
-          image: ExactAssetImage('assets/images/cart.png'),
-          fit: BoxFit.fitHeight,
-          ),
-          ),
-          ),
-          Container(
-          width: 67,
-          height: 70,
-          decoration: new BoxDecoration(
-          image: new DecorationImage(
-          image: ExactAssetImage('assets/images/user.png'),
-          fit: BoxFit.fitHeight,
-          ),
-          ),
-          ),Container(
-          width: 66,
-          height: 56,
-          decoration: new BoxDecoration(
-          image: new DecorationImage(
-          image: ExactAssetImage('assets/images/watsapp.png'),
-          fit: BoxFit.fitHeight,
-          ),
-          ),
-          ),
+            Container(
+              //color: Colors.green,
+              child: const Text("loja"),
+            ),
+            Container(
+              //color: Colors.blue,
+              child: const Text("sobre nos"),
+            ),
+            Container(
+              width: 73,
+              height: 76,
+              decoration: new BoxDecoration(
+                image: new DecorationImage(
+                  image: ExactAssetImage('assets/images/cart.png'),
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+            ),
+            Container(
+              width: 67,
+              height: 70,
+              decoration: new BoxDecoration(
+                image: new DecorationImage(
+                  image: ExactAssetImage('assets/images/user.png'),
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+            ),
+            Container(
+              width: 66,
+              height: 56,
+              decoration: new BoxDecoration(
+                image: new DecorationImage(
+                  image: ExactAssetImage('assets/images/watsapp.png'),
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+            ),
           ],
-          )
-          );
+        ));
   }
 }
--- fabrica_de_dados/lib/screens/home/components/carrosel.dart
+++ fabrica_de_dados/lib/screens/home/components/carrosel.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
@@ -13,92 +15,97 @@ class _CarroselState extends State<Carrosel> {
   CarouselController buttonCarouselController = CarouselController();
 
   final List<String> images = [
-    "../../assets/images/imagem-preta.jpg",
-    "../../assets/images/imagem-rosa.png",
-    "../../assets/images/imagem-azul.png"
+    "../../assets/images/dice1.jpeg",
+    "../../assets/images/dice2.jpg",
+    "../../assets/images/dice3.jpg",
+    "../../assets/images/dice4.jpg"
   ];
 
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-      Center(
-        child: CarouselSlider(
-          carouselController: buttonCarouselController,
-          options: CarouselOptions(
-            height: 400,
-            autoPlay: true,
-            autoPlayInterval: const Duration(seconds: 10),
-            enlargeCenterPage: true,
-            onPageChanged: (index, reason) {
-              setState(() {
-                _current = index;
-              });
-            },
-          ),
-          items: images
-              .map((e) => Stack(children: [
-                    ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Stack(
-                          fit: StackFit.expand,
-                          children: <Widget>[
-                            Image.network(e,
-                                width: 1050, height: 350, fit: BoxFit.cover)
-                          ],
-                        )),
-                    Positioned(
-                        left: MediaQuery.of(context).size.width / 40,
-                        top: MediaQuery.of(context).size.height / 4,
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.white)),
-                          onPressed: () =>
-                              buttonCarouselController.previousPage(
-                                  duration: const Duration(milliseconds: 300),
-                                  curve: Curves.linear),
-                          child: const Icon(
-                            Icons.arrow_back_ios_new_outlined,
-                            color: Colors.black,
-                            size: 36.0,
-                          ),
-                        )),
-                    Positioned(
-                        left: MediaQuery.of(context).size.width / 1.37,
-                        top: MediaQuery.of(context).size.height / 4,
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.white)),
-                          onPressed: () => buttonCarouselController.nextPage(
-                              duration: const Duration(milliseconds: 300),
-                              curve: Curves.linear),
-                          child: const Icon(
-                            Icons.arrow_forward_ios_outlined,
-                            color: Colors.black,
-                            size: 36.0,
-                          ),
-                        ))
-                  ]))
-              .toList(),
+      CarouselSlider(
+        carouselController: buttonCarouselController,
+        options: CarouselOptions(
+          height: 500,
+          autoPlay: true,
+          autoPlayInterval: const Duration(seconds: 7),
+          enlargeCenterPage: true,
+          viewportFraction: 1,
+          onPageChanged: (index, reason) {
+            setState(() {
+              _current = index;
+            });
+          },
         ),
+        items: images
+            .map((e) => Stack(children: [
+                  ClipRRect(
+                      child: Stack(
+                    fit: StackFit.loose,
+                    children: <Widget>[
+                      Image.network(e,
+                          width: 2050, height: 700, fit: BoxFit.cover)
+                    ],
+                  )),
+                  Positioned(
+                      left: MediaQuery.of(context).size.width / 300,
+                      top: MediaQuery.of(context).size.height / 3.25,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white)),
+                        onPressed: () => buttonCarouselController.previousPage(
+                            duration: const Duration(milliseconds: 500),
+                            curve: Curves.linear),
+                        child: const Icon(
+                          Icons.arrow_back_ios_new_outlined,
+                          color: Colors.black,
+                          size: 36.0,
+                        ),
+                      )),
+                  Positioned(
+                      left: MediaQuery.of(context).size.width / 1.05,
+                      top: MediaQuery.of(context).size.height / 3.25,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white)),
+                        onPressed: () => buttonCarouselController.nextPage(
+                            duration: const Duration(milliseconds: 300),
+                            curve: Curves.linear),
+                        child: const Icon(
+                          Icons.arrow_forward_ios_outlined,
+                          color: Colors.black,
+                          size: 36.0,
+                        ),
+                      ))
+                ]))
+            .toList(),
       ),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: images.map((e) {
-          int index = images.indexOf(e);
-          return Container(
-              width: 8.0,
-              height: 8.0,
-              margin:
-                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: _current == index
-                      ? const Color.fromRGBO(0, 0, 0, 0.9)
-                      : const Color.fromRGBO(0, 0, 0, 0.4)));
-        }).toList(),
-      )
+      Container(
+          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+          decoration: BoxDecoration(
+            color: Colors.amber,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: images.map((e) {
+              int index = images.indexOf(e);
+              return Container(
+                  width: 8.0,
+                  height: 8.0,
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 5.0, horizontal: 4.0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _current == index
+                          ? const Color.fromRGBO(0, 0, 0, 0.9)
+                          : const Color.fromRGBO(0, 0, 0, 0.4)));
+            }).toList(),
+          ))
     ]);
   }
 }
