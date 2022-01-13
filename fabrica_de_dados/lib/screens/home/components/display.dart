50e507ee8d8dfac65732eb5de50aaf864738af73
Marcelo Neves <marcelosn2@al.insper.edu.br>
2022-01-12 22:30:52 -0300

    conteiners basicos na tela principal

---
+++ fabrica_de_dados/lib/screens/home/components/display.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Display extends StatelessWidget {
+  const Display({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+              height: 300,
+              width: 270,
+              color: Colors.purple[200],
+              child: Column(
+                children: [
+                  Container(child: const Text('imagem 1')),
+                  Container(child: const Text('item 1'))
+                ],
+              )),
+          Container(
+              height: 300,
+              width: 270,
+              color: Colors.purple[200],
+              child: Column(
+                children: [
+                  Container(child: const Text('imagem 2')),
+                  Container(child: const Text('item 2'))
+                ],
+              )),
+          Container(
+              height: 300,
+              width: 270,
+              color: Colors.purple[200],
+              child: Column(
+                children: [
+                  Container(child: const Text('imagem 3')),
+                  Container(child: const Text('item 3'))
+                ],
+              )),
+        ],
+      ),
+    );
+  }
+}
